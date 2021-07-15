8fcef54a8a99c255540019824e0cefd12753d6dc
henmeh <henningmehnert@gmail.com>
2021-07-15 18:13:25 +0200

    complete bridging and swapping functionality done, following transactions on chain still to do

--- web/blockchainlogic.js
+++ web/blockchainlogic.js
@@ -77,8 +77,11 @@ async function getQuote(_fromToken, _toToken, _amount, _chain) {
     } catch (error) { console.log(error); }
 }
 
-async function _swapEth(_amount) {
+async function _bridgingEth(_amount, _fromChain, _toChain) {
     try {
+        console.log(_fromChain);
+        console.log(_toChain);
+        let chain = [5, 5, 80001];
         user = await Moralis.User.current();
         const _userAddress = user.attributes.ethAddress;
 
@@ -96,24 +99,29 @@ async function _swapEth(_amount) {
             maticProvider: window.web3,
         });
 
-        console.log("do eth bridging");
-
-        //Deposit Ether from Ether to Polygon
-        let txHash = await maticPos.depositEtherForUser(_userAddress, _amount, { from: _userAddress });
-        let deposit = false
-
-        while (!deposit) {
-            deposit = await depositCompletedEth(txHash.transactionHash);
+        if (_fromChain == 0 && _toChain == 2) {
+            //Deposit Ether from Ether to Polygon
+            let txHash = await maticPos.depositEtherForUser(_userAddress, _amount, { from: _userAddress });
+            let deposit = false
+            while (!deposit) {
+                deposit = await depositCompletedEth(txHash.transactionHash);
+            }
+            return deposit;
         }
-
-        console.log("now do swapping on 1inch");
-        return deposit;
-
+        else if (_fromChain == 2 && _toChain == 0) {
+            console.log("Third check");
+            //Deposit Ether from Polygon to Ether
+            let burnTxHash = await maticPosEthBack.burnERC20("0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa", _amount, { from: _userAddress });
+            await checkInclusion(burnTxHash.transactionHash, "0x2890ba17efe978480615e330ecb65333b880928e");
+            await _networkCheck(chain[_toChain]);
+            await maticPos.exitERC20(burnTxHash.transactionHash, { from: _userAddress });
+        }
     } catch (error) { console.log(error); }
 }
 
-async function _swapMatic(_amount) {
+async function _bridgingMatic(_amount) {
     try {
+        let chain = [5, 5, 80001];
         user = await Moralis.User.current();
         const _userAddress = user.attributes.ethAddress;
 
@@ -132,8 +140,6 @@ async function _swapMatic(_amount) {
             maticProvider: window.web3,
         });
 
-        console.log("do matic bridging");
-
         //Approve Polygon
         await maticPlasma.approveERC20TokensForDeposit("0x499d11E0b6eAC7c0593d8Fb292DCBbF815Fb29Ae", _amount, { from: _userAddress });
         //Deposit ERC20
@@ -144,176 +150,110 @@ async function _swapMatic(_amount) {
             deposit = await depositCompletedMatic(txHash.transactionHash);
         }
 
-        console.log("now do swapping on 1inch");
         return deposit;
 
     } catch (error) { console.log(error); }
 }
 
+async function _doSwap(_fromTokenAddress, _toTokenAddress, _amount, _fromChain) {
+    let chain = ["1", "56", "137"];
+
+    if (await window.web3.eth.net.getId() != chain[_fromChain]) {
+        alert("Please Change Network on Metamask to the right Network and than press OK");
+    }
+
+    window.ERC20TokencontractInstance = new web3.eth.Contract(erc20ABI, _fromTokenAddress);
+    //Approve 1inch to spend token
+    if (_fromTokenAddress != "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
+        const allowance = await ERC20TokencontractInstance.methods.allowance(_userAddress, "0x11111112542d85b3ef69ae05771c2dccff4faa26").call();
+        if (allowance < web3.utils.toBN(parseFloat(_amount))) {
+            const approve = await ERC20TokencontractInstance.methods.approve("0x11111112542d85b3ef69ae05771c2dccff4faa26", _amount).send({ from: _userAddress });
+        }
+    }
+    //get TX Data for swap to sign and to do the swap            
+    const response = await fetch(`https://api.1inch.exchange/v3.0/${chain[_fromChain]}/swap?fromTokenAddress=${_fromTokenAddress}&toTokenAddress=${_toTokenAddress}&amount=${_amount}&fromAddress=${_userAddress}&slippage=1`);
+    const swap = await response.json();
+    const send = await web3.eth.sendTransaction(swap.tx);
+    return send;
+}
+
+async function _networkCheck(_networkId) {
+    let network = await window.web3.eth.net.getId();
+    console.log(_networkId);
+    console.log(network);
+    if (network != _networkId) {
+        alert("Please Change Network in Metamask and then press OK");
+    }
+}
+
 async function swap(_fromTokenAddress, _toTokenAddress, _amount, _fromChain, _toChain) {
+    let chain = [5, 5, 80001];
 
-    user = await Moralis.User.current();
-    const _userAddress = user.attributes.ethAddress;
+    console.log(_fromTokenAddress);
+    console.log(_toTokenAddress);
+    console.log(_fromChain);
+    console.log(_toChain);
 
-    const maticPos = new Matic.MaticPOSClient({
-        network: "testnet", //"mainnet",
-        version: "mumbai", //"v1",
-        parentProvider: window.web3,
-        maticProvider: "https://speedy-nodes-nyc.moralis.io/cff6f789838e10c4008b1baa/polygon/mumbai", //"https://speedy-nodes-nyc.moralis.io/cff6f789838e10c4008b1baa/polygon/mainnet",
-    });
-
-    const maticPosEthBack = new Matic.MaticPOSClient({
-        network: "testnet", //"mainnet",
-        version: "mumbai", //"v1",
-        parentProvider: "https://speedy-nodes-nyc.moralis.io/cff6f789838e10c4008b1baa/eth/goerli", //"https://speedy-nodes-nyc.moralis.io/cff6f789838e10c4008b1baa/eth/mainnet",
-        maticProvider: window.web3,
-    });
-
-    let polygonnetwork = await window.web3.eth.net.getId();
-    if (polygonnetwork != 80001) {
-        alert("Please Change Network to Mumbai Testnet and than press OK");
+    if (_fromChain == _toChain) {
+        await _networkCheck(chain[_fromChain]);
+        //Direct swap with 1inch
+        //await _doSwap(_fromTokenAddress, _toTokenAddress, _amount, _fromChain);
+        console.log("Do direct swap");
     }
 
-    let burnTxHash = await maticPosEthBack.burnERC20("0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa", "10000000000000000", { from: _userAddress });
-
-    //switching network in metamask from polygon to eth
-
-    //let ethnetwork = await window.web3.eth.net.getId();
-    //if (ethnetwork != 5) {
-    //    alert("Please Change Network to Goerli Testnet and than press OK");
-    //}
-    //waiting until burn transaction has been checkpointed
-    //let message = "error";
-
-    await checkInclusion(burnTxHash.transactionHash, "0x2890ba17efe978480615e330ecb65333b880928e")
-
-
-    //while (message != "success") {
-    //    const url = `https://apis.matic.network/api/v1/mumbai/block-included/${burnTxHash.blockNumber}`;
-    //    let response = await fetch(url);
-    //    let data = await response.json();
-    //    message = data["message"];
-    //}
-    //console.log(message)
-
-    //switching network in metamask from polygon to eth
-
-    let ethnetwork = await window.web3.eth.net.getId();
-    if (ethnetwork != 5) {
-        alert("Please Change Network to Goerli Testnet and than press OK");
+    else {
+        if (_fromChain == 0 && _toChain == 2) {
+            //Check if the FromToken is ETH or Matic, beacause these will be bridged directly
+            if (_fromTokenAddress == "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
+                await _networkCheck(chain[_fromChain]);
+                await _bridgingEth(_amount, _fromChain, _toChain);
+                let _newFromTokenAddress = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+                await _networkCheck(chain[_toChain]);
+                //await _doSwap(_newFromTokenAddress, _toTokenAddress, _amount, _toChain);
+                console.log("Do swap");
+            }
+            else if (_fromTokenAddress == "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0".toLowerCase()) {
+                await _networkCheck(chain[_fromChain]);
+                await _bridgingMatic(_amount);
+                let _newFromTokenAddress = "0x0000000000000000000000000000000000001010";
+                await _networkCheck(chain[_toChain]);
+                //await _doSwap(_newFromTokenAddress, _toTokenAddress, _amount, _toChain);
+                console.log("Do swap");
+            }
+            //all other FromTokens will first be swapped into Eth on Eth and then bridged to Polygon and again swapped into the final Token on Polygon
+            else {
+                await _networkCheck(chain[_fromChain]);
+                await _doSwap(_fromTokenAddress, "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", _amount, _fromChain);
+                //getting the swapped amount in eth
+                let _ethBridgingAmount = "";
+                await _bridgingEth(_ethBridgingAmount, _fromChain, _toChain);
+                let _newFromTokenAddress = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+                await _networkCheck(chain[_toChain]);
+                await _doSwap(_newFromTokenAddress, _toTokenAddress, _ethBridgingAmount, _toChain);
+            }
+        }
+        else if (_fromChain == 2 && _toChain == 0) {
+            //Check if FromToken is WETH this will be bridged directly
+            if (_fromTokenAddress == "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619".toLowerCase()) {
+                await _networkCheck(chain[_fromChain]);
+                await _bridgingEth(_amount, _fromChain, _toChain);
+                let _newFromTokenAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+                await _networkCheck(chain[_toChain]);
+                //await _doSwap(_newFromTokenAddress, _toTokenAddress, _amount, _toChain);
+                console.log("Do swap");
+            }
+            else {
+                await _networkCheck(chain[_fromChain]);
+                await _doSwap(_fromTokenAddress, "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619".toLowerCase(), _amount, _fromChain);
+                //getting the swapped amount in eth
+                let _ethBridgingAmount = "";
+                await _bridgingEth(_ethBridgingAmount, _fromChain, _toChain);
+                let _newFromTokenAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+                await _networkCheck(chain[_toChain]);
+                await _doSwap(_newFromTokenAddress, _toTokenAddress, _ethBridgingAmount, _toChain);
+            }
+        }
     }
-
-    await maticPos.exitERC20(burnTxHash.transactionHash, {from: _userAddress});
-
-    //if (_fromTokenAddress == "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
-    //    let swap = await _swapEth(_amount);
-    //    console.log(swap);
-    //    return swap;
-    //}
-
-    //else if (_fromTokenAddress == "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0".toLowerCase()) {
-    //    let swap = await _swapMatic(_amount);
-    //    console.log(swap);
-    //    return swap;
-    //}
-
-
-    //POS
-    //Deposit DummyERC20 rom Ether to Polygon
-    //Approve Polygon
-    //await maticPos.approveERC20ForDeposit("0x655F2166b0709cd575202630952D71E2bB0d61Af", "1000000000000000000", { from: _userAddress });
-    //Deposit ERC20
-    //let txHash = await maticPos.depositERC20ForUser("0x655F2166b0709cd575202630952D71E2bB0d61Af", _userAddress, "1000000000000000000", { from: _userAddress });
-
-    //Get Token from Polygon to Ether 
-    //Plasma
-    //await maticPlasmaEthBack.startWithdraw("0x0000000000000000000000000000000000001010", "1000000000000000000", { from: _userAddress });
-    //await maticPlasmaEthBack.processExits("0x0000000000000000000000000000000000001010", { from: _userAddress });
-
-    //Get Token from Polygon to Ether 
-    //POS
-    //await maticPosEthBack.burnERC20("0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa","100000000000000000", { from: _userAddress });
-    //await maticPlasmaEthBack.processExits("0x0000000000000000000000000000000000001010", { from: _userAddress });
-
-    /*        console.log(_fromTokenAddress);
-            console.log(_toTokenAddress);
-            console.log(_amount);
-            console.log(_fromChain);
-            console.log(_toChain);
-    
-            let chain = ["1", "56", "137"];
-    
-            //direct swap without any token bridging
-            if (_fromChain == _toChain) {
-                let network = await web3.eth.net.getId();
-                if (network != chain[_fromChain]) {
-                    alert("Please switch network");
-                    return;
-                }
-                window.ERC20TokencontractInstance = new web3.eth.Contract(erc20ABI, _fromTokenAddress);
-                //Approve 1inch to spend token
-                if (_fromTokenAddress != "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
-                    const allowance = await ERC20TokencontractInstance.methods.allowance(_userAddress, "0x11111112542d85b3ef69ae05771c2dccff4faa26").call();
-                    if (allowance < web3.utils.toBN(parseFloat(_amount))) {
-                        const approve = await ERC20TokencontractInstance.methods.approve("0x11111112542d85b3ef69ae05771c2dccff4faa26", _amount).send({ from: _userAddress });
-                    }
-                }
-                //get TX Data for swap to sign and to do the swap            
-                const response = await fetch(`https://api.1inch.exchange/v3.0/${chain[_toChain]}/swap?fromTokenAddress=${_fromTokenAddress}&toTokenAddress=${_toTokenAddress}&amount=${_amount}&fromAddress=${_userAddress}&slippage=1`);
-                const swap = await response.json();
-                const send = await web3.eth.sendTransaction(swap.tx);
-            }
-    
-            //bridging from ether to polygon
-            if (_fromChain == 0 && _toChain == 2) {
-                let network = await web3.eth.net.getId();
-                let _fromTokenAfterDeposit;
-                if (network != chain[_fromChain]) {
-                    alert("Please switch network");
-                    return;
-                }
-                //Deposit Token on Polygon
-                if (_fromTokenAddress != "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
-                    //Approve Polygon
-                    _fromTokenAddress == "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0" ? await maticPlasma.approveERC20TokensForDeposit(_fromTokenAddress, _amount, { from: _userAddress }) : await maticPos.approveERC20ForDeposit(_fromTokenAddress, _amount, { from: _userAddress });
-                    //Deposit ERC20
-                    _fromTokenAddress == "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0" ? await maticPlasma.depositERC20ForUser(_fromTokenAddress, _userAddress, _amount, { from: _userAddress }) : await maticPos.depositERC20ForUser(_fromTokenAddress, _userAddress, _amount, { from: _userAddress });;
-                    //Approve 1Inch
-                    _fromTokenAfterDeposit = "0x0000000000000000000000000000000000001010";
-                    window.ERC20TokencontractInstance = new web3.eth.Contract(erc20ABI, _fromTokenAfterDeposit);
-                    const approve1 = await ERC20TokencontractInstance.methods.approve("0x11111112542d85b3ef69ae05771c2dccff4faa26", _amount).send({ from: _userAddress });
-                }
-                //Deposit Ether on Polygon
-                else {
-                    await maticPos.depositEtherForUser(_userAddress, _amount, { from: _userAddress });
-                    _fromTokenAfterDeposit = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
-                    //Swich Metamasknetwork to Polygon
-                    alert("Switch Network to Polygon");
-                    window.ERC20TokencontractInstance = new web3.eth.Contract(erc20ABI, _fromTokenAfterDeposit);
-                    const approve1 = await ERC20TokencontractInstance.methods.approve("0x11111112542d85b3ef69ae05771c2dccff4faa26", _amount).send({ from: _userAddress });
-                    //????? Wait until Ether is on Polygon ?????
-                }
-                //Getting swap Data for swap on Polygon     
-                const response = await fetch(`https://api.1inch.exchange/v3.0/${chain[_toChain]}/swap?fromTokenAddress=${_fromTokenAfterDeposit}&toTokenAddress=${_toTokenAddress}&amount=${_amount}&fromAddress=${_userAddress}&slippage=1`);
-                const swap = await response.json();
-                const send = await web3.eth.sendTransaction(swap.tx);
-            }
-                    //bridging from polygon to ether
-                    if (_fromChain == 2 && _toChain == 0) {
-                        //Burn Token
-                        let burnTxHash;
-                        _fromTokenAddress == "0x0000000000000000000000000000000000001010" ? burnTxHash = await maticPlasmaEthBack.startWithdraw(_fromTokenAddress, _amount, { from: _userAddress }) : burnTxHash = await maticPosEthBack.burnERC20(_fromTokenAddress, _amount, { from: _userAddress });
-                        //Exit Token
-                        _fromTokenAddress == "0x0000000000000000000000000000000000001010" ? await maticPlasmaEthBack.processExits(_fromTokenAddress, { from: _userAddress }) : await maticPosEthBack.exitERC20(burnTxHash, { from: _userAddress });;
-                        //Approve 1Inch
-                        const _fromTokenAfterDeposit = "0x0000000000000000000000000000000000001010";
-                        window.ERC20TokencontractInstance = new web3.eth.Contract(erc20ABI, _fromTokenAfterDeposit);
-                        const approve1 = await ERC20TokencontractInstance.methods.approve("0x11111112542d85b3ef69ae05771c2dccff4faa26", _amount).send({ from: _userAddress });
-                        //Getting swap Data for swap on Polygon     
-                        const response = await fetch(`https://api.1inch.exchange/v3.0/${chain[_toChain]}/swap?fromTokenAddress=${_fromTokenAfterDeposit}&toTokenAddress=${_toTokenAddress}&amount=${_amount}&fromAddress=${_userAddress}&slippage=1`);
-                        const swap = await response.json();
-                        const send = await web3.eth.sendTransaction(swap.tx);
-                    }*/
 }
 
 async function checkInclusion(txHash, rootChainAddress) {
@@ -351,7 +291,7 @@ async function checkInclusion(txHash, rootChainAddress) {
                         ["uint256", "uint256", "bytes32"],
                         result.data
                     );
-                    
+
                     if (block <= transaction["1"]) {
                         console.log(transaction);
                         resolve(result);
